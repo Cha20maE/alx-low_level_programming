cf9063e4681161da20efc1fb49908a39e30dd802
Cha20maE <>
2023-04-26 17:43:35 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- *
+ * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 
@@ -15,5 +15,7 @@ int _islower(int c)
 
 	return (1);
 	}
+
 	return (0);
+
 }
